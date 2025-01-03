1c0afa693e779fee3671fbb4df04eaa3d42dc75b
Vinayak-YB <vinayak.bhustali@gmail.com>
2025-01-03 08:33:29 +0530

    test.c opens file and reads data into buffer

--- test.c
+++ test.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main()
 {
     FILE *file;
-    char buffer[0];
+    char *buffer = 0;
+    long length;
 
     file = fopen("test.unn", "r");
-
-    fread(buffer, 1, 1, file);
-    printf("%c\n", buffer);
+    fseek(file, 0, SEEK_END);
+    length = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    buffer = malloc(length + 1);
+    fread(buffer, 1, length, file);
+    
     fclose(file);
+    buffer[length]= '\0';
+    printf("%s\n", buffer);
+
+    char *current = buffer;
+    int current_index = 0;
+    while(current[current_index] != '\0')
+    {
+        printf("%c\n", current[current_index]);
+        current_index++;
+    }
+    return 0;
 }
